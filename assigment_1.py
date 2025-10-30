a25ef49848bda4218c63fe7107878da225407d5f
sahilabbas72 <sahilkhan454573@gmail.com>
2025-10-30 14:35:15 +0530

    Add files via upload

---
+++ assigment_1.py
@@ -0,0 +1,16 @@
+user1 = int(input("Enter the First number :-"))
+user2 = int(input("Enter the Second number:-"))
+
+#task 1
+print("ADDITION OF TWO NUMBERS:-",(user1+user2))
+print("SUBTRACTION OF TWO NUMBERS:-",(user1-user2))
+print("MULTIPLICATION OF TWO NUMBERS:-",(user1*user2))
+print("DIVISION OF TWO NUMBERS:-",(user1/user2))
+
+
+
+#task 2
+
+First_name = (input("Enter First Namer:"))
+Laste_name = (input("Enter your last name:"))
+print(f"Hello,{First_name+" "+Laste_name}! welcom to the python program")
